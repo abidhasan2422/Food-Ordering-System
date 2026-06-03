e72d54dce94a670a869523f2f7c55601a7412cf5
Abid Hasan <likhon2422@gmail.com>
2026-06-03 08:13:30 +0600

    Navbar Design on Home Page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
+import {FaHome, FaSignInAlt, FaTruck, FaUserPlus, FaUserShield, FaUtensils} from 'react-icons/fa'
 const Home = () => {
     return ( 
-        <div > Home Page - Food Ordering system </div>
+        <div > 
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div className="container">
+    <Link className="navbar-brand fw-bold" href="#"> <FaUtensils className='me-2'/>Food Ordering System</Link>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" >
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li className="nav-item mx-1" >
+          <Link className="nav-link active" to="#"><FaHome className='me-2'/>Home</Link>
+        </li>
+        <li className="nav-item mx-1">
+          <Link className="nav-link" to="#"> <FaTruck className='me-2'/> Track</Link>
+        </li>
+        <li className="nav-item mx-1">
+          <Link className="nav-link" to="#"> <FaUserPlus className='me-2'/> Register</Link>
+        </li>
+      
+        <li className="nav-item mx-1">
+          <Link className="nav-link" to="#"> <FaSignInAlt className='me-2'/> Login</Link>
+        </li>
+        <li className="nav-item mx-1">
+          <Link className="nav-link" to="#"> <FaUserShield className='me-2'/> AdminLogin</Link>
+        </li>
+       
+      </ul>
+     
+    </div>
+  </div>
+</nav>
+        </div>
     )
 }
 
