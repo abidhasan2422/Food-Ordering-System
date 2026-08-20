3434fcaac8fe61bb47cddecf86e4ce2b3f5af76f
Abid Hasan <likhon2422@gmail.com>
2026-08-20 16:15:51 +0600

    update setting for deploy

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,7 +1,6 @@
 
-
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -9,13 +8,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ie_uo4qmcra=y(p)ikkh21fj409cg0z%bd@6&+!9&37r8&-n#i'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    ".onrender.com",
+]
 
 
 # Application definition
@@ -36,16 +37,24 @@ INSTALLED_APPS = [
     
 
 ]
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = False
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
+
+
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'backend.urls'
@@ -119,11 +128,11 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -180,13 +189,20 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = "likhon2422@gmail.com"
-EMAIL_HOST_PASSWORD = "budj tgos xawt dtah"
-
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
-SSLCOMMERZ_STORE_ID = "alsso6a4956a47634f"
-SSLCOMMERZ_STORE_PASSWORD = "alsso6a4956a47634f@ssl"
-SSLCOMMERZ_SANDBOX = True
-SSLCOMMERZ_API_URL = "https://sandbox.sslcommerz.com/gwprocess/v4/api.php"
+SSLCOMMERZ_STORE_ID = os.environ.get("SSLCOMMERZ_STORE_ID")
+SSLCOMMERZ_STORE_PASSWORD = os.environ.get("SSLCOMMERZ_STORE_PASSWORD")
+
+SSLCOMMERZ_SANDBOX = os.environ.get(
+    "SSLCOMMERZ_SANDBOX",
+    "True"
+) == "True"
+
+SSLCOMMERZ_API_URL = os.environ.get(
+    "SSLCOMMERZ_API_URL",
+    "https://sandbox.sslcommerz.com/gwprocess/v4/api.php"
+)
